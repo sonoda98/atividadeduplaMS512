a853d1d52a08ace9ff11b6b29342ffc04e4dedfd
caiovinissouza <104103553+scaioz@users.noreply.github.com>
2024-11-25 16:34:20 -0300

    Add files via upload

---
+++ segunda_questao/resolucao_ex14_questao2.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Criando uma matriz A (n x m) e um vetor b
+np.random.seed(42)
+n, m = 5, 3  # Dimensão de A (n > m para sistema sobredeterminado)
+A = np.random.rand(n, m)
+b = np.random.rand(n)
+
+# Calculando a pseudoinversa de A
+A_pseudo_inv = np.linalg.pinv(A)
+
+# Solução de norma mínima
+x = A_pseudo_inv @ b
+
+# Verificando a norma mínima (resíduo ||b - Ax||_2)
+residuo = np.linalg.norm(b - A @ x, ord=2)
+
+print(A_pseudo_inv)
+print(x)
+print(residuo)
